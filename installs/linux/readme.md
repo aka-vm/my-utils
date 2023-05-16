6877ccb9304a39aa15683a4cc58817406e7d4ff8
aka-vm <vineetmahajan2000@gmail.com>
2023-05-16 17:50:11 +0530

    zsh

---
+++ installs/linux/readme.md
@@ -0,0 +1,8 @@
+# Common Linux Install Instructions
+
+## Install Dependencies
+
+```sh
+sudo apt-get update
+sudo apt-get upgrade
+```
---
+++ installs/linux/zsh_init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ask password
+sudo -v
+# zsh
+sudo apt-get update
+sudo apt-get install zsh
+chsh -s $(which zsh)
+
+# oh-my-zsh
+sudo apt-get install git curl
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# set  ZSH_THEME="powerlevel10k/powerlevel10k” to .zshrc
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+
